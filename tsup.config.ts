48faaba63c6fec78f11d7b5190475dcea7040a49
liuyu2 <liuyu2@duiba.com.cn>
2022-05-26 10:14:30 +0800

    fix: cjs error

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+  ],
+  format: [
+    'esm',
+    'cjs',
+  ],
+  dts: true,
+  clean: true,
+  splitting: true,
+})
